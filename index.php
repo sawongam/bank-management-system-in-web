e153453988a69a6c6becc4c1f56c208949eb1cec
Sangam Adhikari <sangamadhikari.61@gmail.com>
2023-09-19 14:56:29 +0545

    Temporary basic login System

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bank Management System by Sawongam</title>
+    <title>Bank Management System by sawongam</title>
 </head>
+
 <body>
     <form action="loginAuth.php" method="POST">
         <input type="text" name="username" placeholder="Username">
         <input type="password" name="password" placeholder="Password">
-        <input type="submit" value="Login">
+        <input type="submit" name="submit" value="Login">
     </form>
-    
+
 </body>
+
 </html>
--- loginAuth.php
+++ loginAuth.php
@@ -1,18 +1,28 @@
 <?php
 $host = "localhost";
 $dbusername = "root";
-$dbpassword = ""; 
+$dbpassword = "";
 $databasename = "bms";
-$conn = mysqli_connect($host,$dbusername,$dbpassword,$databasename);
+$conn = mysqli_connect($host, $dbusername, $dbpassword, $databasename);
+
+if (!$conn) {
+   die("Connection failed: " . mysqli_connect_error());
+}
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 $submitted = $_POST['submit'];
 
-if($submitted) {
-    
-} else {
-    echo("s");
+if ($submitted) {
+   $sql = "SELECT * from credentials where AccNo = '$username' AND Pass = '$password'";
+   $result = mysqli_query($conn, $sql);
+   $countUserPass = mysqli_num_rows($result);
+   if ($countUserPass == 1) {
+    header('Location: dashboard.php');
+    exit;
+   }
+   else echo "failed";
 }
 
 ?>
+
