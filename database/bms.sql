41a1663b47159c5a992543e6451426bb1e9b9ed2
Sangam Adhikari <sangamadhikari.61@gmail.com>
2024-12-19 09:33:17 +0545

    update: database dump updated

--- database/bms.sql
+++ database/bms.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 30, 2024 at 04:38 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 7.4.27
+-- Generation Time: Dec 19, 2024 at 04:47 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,17 +31,17 @@ CREATE TABLE `balance` (
   `AccNo` int(11) NOT NULL,
   `Balance` decimal(15,0) DEFAULT NULL,
   `Interest` decimal(15,0) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `balance`
 --
 
 INSERT INTO `balance` (`AccNo`, `Balance`, `Interest`) VALUES
-(197, '35', '2'),
-(198, '5', '0'),
-(199, '40', '2'),
-(200, '212', '12');
+(197, 35, 2),
+(198, 5, 0),
+(199, 45, 2),
+(200, 207, 12);
 
 -- --------------------------------------------------------
 
@@ -52,7 +52,7 @@ INSERT INTO `balance` (`AccNo`, `Balance`, `Interest`) VALUES
 CREATE TABLE `credentials` (
   `AccNo` int(11) NOT NULL,
   `Pass` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `credentials`
@@ -75,31 +75,31 @@ CREATE TABLE `transactions` (
   `Receiver` int(11) NOT NULL,
   `Amount` decimal(10,0) NOT NULL,
   `Remarks` varchar(50) NOT NULL,
-  `Date` date NOT NULL DEFAULT current_timestamp(),
-  `Time` time NOT NULL DEFAULT current_timestamp(),
+  `DateTime` datetime NOT NULL DEFAULT current_timestamp(),
   `SenBalance` decimal(10,0) NOT NULL,
   `RecBalance` decimal(10,0) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `transactions`
 --
 
-INSERT INTO `transactions` (`Sender`, `Receiver`, `Amount`, `Remarks`, `Date`, `Time`, `SenBalance`, `RecBalance`) VALUES
-(200, 197, '10', 'Hired as Accountant', '2024-01-28', '15:46:17', '59', '79'),
-(200, 198, '10', 'Hired as Sales Manager', '2024-01-28', '15:47:02', '49', '79'),
-(200, 199, '30', 'Hired as Operation Manager', '2024-01-28', '15:47:37', '19', '99'),
-(200, 199, '19', 'Investment', '2024-01-28', '15:48:40', '0', '118'),
-(197, 200, '10', 'First Income', '2024-01-28', '15:49:17', '69', '10'),
-(198, 197, '40', 'ROI Again', '2024-01-28', '15:50:00', '39', '109'),
-(198, 197, '30', 'Miscellaneous Costs', '2024-01-28', '15:50:22', '9', '139'),
-(199, 197, '30', 'Sales Income', '2024-01-28', '15:50:47', '88', '169'),
-(199, 200, '20', 'Advetisers Payout', '2024-01-28', '15:51:14', '68', '30'),
-(197, 200, '100', 'Regular Income now', '2024-01-28', '15:51:36', '69', '130'),
-(197, 200, '50', 'More Sales', '2024-01-28', '15:51:55', '19', '180'),
-(199, 200, '30', 'Providers Funding', '2024-01-28', '15:52:40', '38', '210'),
-(200, 197, '10', 'Tax Cuts', '2024-01-28', '15:54:24', '200', '29'),
-(198, 197, '4', 'All remaining Bal', '2024-01-28', '22:13:47', '5', '33');
+INSERT INTO `transactions` (`Sender`, `Receiver`, `Amount`, `Remarks`, `DateTime`, `SenBalance`, `RecBalance`) VALUES
+(200, 197, 10, 'Hired as Accountant', '2024-12-19 15:46:17', 59, 79),
+(200, 198, 10, 'Hired as Sales Manager', '2024-12-19 15:47:02', 49, 79),
+(200, 199, 30, 'Hired as Operation Manager', '2024-12-19 15:47:37', 19, 99),
+(200, 199, 19, 'Investment', '2024-12-19 15:48:40', 0, 118),
+(197, 200, 10, 'First Income', '2024-12-19 15:49:17', 69, 10),
+(198, 197, 40, 'ROI Again', '2024-12-19 15:50:00', 39, 109),
+(198, 197, 30, 'Miscellaneous Costs', '2024-12-19 15:50:22', 9, 139),
+(199, 197, 30, 'Sales Income', '2024-12-19 15:50:47', 88, 169),
+(199, 200, 20, 'Advetisers Payout', '2024-12-19 15:51:14', 68, 30),
+(197, 200, 100, 'Regular Income now', '2024-12-19 15:51:36', 69, 130),
+(197, 200, 50, 'More Sales', '2024-12-19 15:51:55', 19, 180),
+(199, 200, 30, 'Providers Funding', '2024-12-19 15:52:40', 38, 210),
+(200, 197, 10, 'Tax Cuts', '2024-12-19 15:54:24', 200, 29),
+(198, 197, 4, 'All remaining Bal', '2024-12-19 22:13:47', 5, 33),
+(200, 199, 5, 'Turn Over Profits', '2024-12-19 09:21:59', 207, 45);
 
 -- --------------------------------------------------------
 
@@ -112,7 +112,7 @@ CREATE TABLE `userinfo` (
   `Name` varchar(50) DEFAULT NULL,
   `Address` varchar(100) DEFAULT NULL,
   `Email` varchar(64) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `userinfo`
